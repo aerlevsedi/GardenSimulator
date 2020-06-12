73e718eb53c59bfbd5dc623623b5673947eae0a9
Marta Sobas <marta.sobas@hotmail.com>
2020-06-13 00:34:13 +0200

    Elch2

--- src/main/java/garden_simulator/app/Main.java
+++ src/main/java/garden_simulator/app/Main.java
@@ -20,6 +20,5 @@ public class Main {
 
         GardenProperties gardenProperties = new GardenProperties(gardenDimensions, maxPlantsNumber);
         simulation.run(gardenProperties);
-        //sdsds
     }
 }
